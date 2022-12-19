aa0d8064073c942a93ea56a471e2dd236c87fe7a
Cody <cemt5462@gmail.com>
2022-12-19 10:54:03 -0600

    Created custom dictionary response for getting all supers

--- supers/views.py
+++ supers/views.py
@@ -13,10 +13,20 @@ def all_supers(request):
     if request.method == 'GET':
         super_type = request.query_params.get('super_type') #Gets value from a query parameter sent through the url. 
         super = Super.objects.all()
-        if super_type:
+        if super_type: #If super_type has a value then we will enter the if statement.
             super = super.filter(super_type__type=super_type) #Filters the supers to return the type requested in the url
-        serializer = SuperSerializers(super, many=True)
-        return Response(serializer.data)
+            serializer = SuperSerializers(super, many=True)
+            return Response(serializer.data)
+        else:
+            heroes = Super.objects.filter(super_type_id = 1)
+            hero_serializer = SuperSerializers(heroes, many=True)
+            villians = Super.objects.filter(super_type_id = 2)
+            villian_serializer = SuperSerializers(villians, many=True)
+            custom_response = {
+                'Heroes': hero_serializer.data,
+                'Villains': villian_serializer.data
+            }
+        return Response(custom_response)
     elif request.method == 'POST':
         serializer = SuperSerializers(data=request.data)
         serializer.is_valid(raise_exception=True)
